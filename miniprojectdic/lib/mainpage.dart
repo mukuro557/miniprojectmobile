587839bebc9113f829acfb6be35b8c8ccee9c3e6
Anutthap Taynagm <bnbn_159@hotmail.com>
2020-11-23 20:32:19 +0700

    add homepage

---
+++ miniprojectdic/lib/mainpage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Mainpagedictionary extends StatefulWidget {
+  @override
+  _MainpagedictionaryState createState() => _MainpagedictionaryState();
+}
+
+class _MainpagedictionaryState extends State<Mainpagedictionary> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
